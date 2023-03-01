f74d4aa41ac8a70e26080765e2b6ac8ace3211c6
Rebecca <119697291+rbrantley0414@users.noreply.github.com>
2023-03-01 16:42:10 -0600

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -69,6 +69,9 @@
 //         .catch(error => console.log('error', error));
 // }
 
+var obj;
+var output = '';
+
 function getCurrency() {
     var myHeaders = new Headers();
     myHeaders.append("apikey", "0D7KYS4a1T01b6u32SYYyktfF8OY0Nly");
@@ -79,19 +82,19 @@ function getCurrency() {
         headers: myHeaders
     };
 
-    var obj;
+    // var obj;
 
-    fetch("https://api.apilayer.com/fixer/symbols", requestOptions)
+    fetchSymbols("https://api.apilayer.com/fixer/symbols", requestOptions)
         .then(res => res.json())
         .then(data => {
-            obj = data.symbols;
+            return obj = data.symbols;
         })
         .then(() => {
             // console.log(obj);
             // console.log(Object.keys(obj));
             // console.log(Object.values(obj));
             let output = `<option>Currencies</option>`;
-            // loop for grabbing each country name and displaying it in drop down
+            // loop for grabbing each currency name and displaying it in drop down
             Object.values(obj).forEach(val => {
                 output += `
                 <option>${val}</option>
@@ -105,30 +108,48 @@ function getCurrency() {
 // use an onchange event
 
 function convertCurrency() {
-    var myHeaders = new Headers();
-    myHeaders.append("apikey", "0D7KYS4a1T01b6u32SYYyktfF8OY0Nly");
+    var selectedElement = document.getElementById("c");
+    var value = selectedElement.value;
 
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow',
-        headers: myHeaders
-    };
+    function getKeyByValue(obj, value) {
+        return Object.keys(obj).find(key => obj[key] === value);
+        console.log(key)
+    }
 
-    var currency1
-    var currency2
-    var amount
-    var currency1value = document.querySelector(".currency1value")
-    var currency2value = document.querySelector(".currency2value")
-
-    fetch("https://api.apilayer.com/fixer/convert?to="
-        + currency1
-        + "&from="
-        + currency2
-        + "&amount="
-        + amount, requestOptions)
-        .then(response => response.json())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+    //   console.log(getKeyByValue(obj, value));
 }
 
-// Object.keys(obj)
+document.querySelector(".inputNum").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        convertCurrency();
+    }
+});
+
+
+// function convertCurrency() {
+//     var myHeaders = new Headers();
+//     myHeaders.append("apikey", "0D7KYS4a1T01b6u32SYYyktfF8OY0Nly");
+
+//     var requestOptions = {
+//         method: 'GET',
+//         redirect: 'follow',
+//         headers: myHeaders
+//     };
+
+//     var currency1
+//     var currency2
+//     var amount
+//     var currency1value = document.querySelector(".currency1value")
+//     var currency2value = document.querySelector(".currency2value")
+
+//     fetchConvert("https://api.apilayer.com/fixer/convert?to="
+//         + currency1
+//         + "&from="
+//         + currency2
+//         + "&amount="
+//         + amount, requestOptions)
+//         .then(response => response.json())
+//         .then(result => console.log(result))
+//         .catch(error => console.log('error', error));
+// }
+
